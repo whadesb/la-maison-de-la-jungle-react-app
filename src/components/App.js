25f61e566d343c582b31ba56e4df642ec00a8bc9
whadesb <thomas.bouez@gmail.com>
2022-11-15 11:30:36 +0100

    La maison de la jungle React App OpenClassRoom v.001

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "../styles/index.css"
+import Banner from "./Banner";
+import Cart from "./Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+import "../styles/Banner.css"
+import logo from "../assets/logo.png"
+
+
+const Banner = () => {
+    const title = 'La maison de la jungle'
+
+    return (
+        <div className="lmj-banner">
+            <img src={logo} alt="La maison jungle" className="lmj-logo" />
+            <h1 className="lmj-title">{title}</h1>
+        </div>
+    );
+}
+ 
+export default Banner;
